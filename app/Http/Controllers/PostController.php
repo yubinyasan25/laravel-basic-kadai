8791a043e2690bacc8948c31252c2716944ed00b
yubinyasan25 <yubin.yasan.27@gmail.com>
2025-09-12 11:28:00 +0900

    課題提出

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -12,5 +13,11 @@ class PostController extends Controller
         $posts = DB::table('posts')->get();
 
         return view('posts.index',compact('posts'));
+    }
+    public function show($id) {
+        
+        $post = Post::find($id);
+
+        return view('posts.show', compact('post'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/', function () {
 
 
 Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
